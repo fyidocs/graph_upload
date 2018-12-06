90cbc8114a34f98a8025faf752de289347b85cf9
Syed Ahmed Anas <ahmed.a.tamim@gmail.com>
2018-12-06 13:22:29 +0500

    added isValid flag and validation function

--- index.js
+++ index.js
@@ -5,9 +5,14 @@ const GraphHelper = require('./lib/graph.helper');
 
 
 async function start() {
+    //computes simple tree structure
     let treeStructure = await GraphHelper.getTreeFormatById(options.start_folder_id);
-   
-    let uploadedFiles = await GraphHelper.uploadValidFolders(treeStructure);
+
+    //adds an isValid flag for each folder in the tree structure
+    let validatedTreeStructure = await GraphHelper.validateTree(treeStructure);
+
+    //uploads files of valid folders
+    let uploadedFiles = await GraphHelper.uploadValidFolders(validatedTreeStructure);
     return uploadedFiles;
 
 
